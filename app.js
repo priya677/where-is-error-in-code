b88a47ddb0a7462a7ea9e20470a7162adb66f97b
priya677 <priyados708@gmail.com>
2023-08-03 14:06:27 +0000

    commit message

---
+++ app.js
@@ -0,0 +1,140 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const databasePath = path.join(__dirname, "cricketMatchDetails.db");
+
+const app = express();
+
+app.use(express.json());
+let database = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    database = await open({
+      filename: databasePath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () =>
+      console.log("Server Running at http://localhost:3000/")
+    );
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+const convertPlayerDetailsDbObjectToResponseObject=(dbObject)=>{
+    return{
+        playerId:dbObject.player_id,
+        playerName:dbObject.player_name,
+    };
+};
+
+const convertMatchDetailsDbObjectToResponseObject=(dbObject)=>{
+    return{
+        matchId:dbObject.match_id,
+        match:dbObject.match,
+        year:dbObject.year,
+    };
+};
+app.get("/players/",async(request,response)=>{
+    const getPlayerQuery=`
+    SELECT
+    *
+    FROM
+      player_details;`;
+    const playerArray=await database.all((getPlayerQuery);
+    response.send(
+      playerArray.map((eachPlayer)=>
+        convertPlayerDetailsDbObjectToResponseObject(eachPlayer)
+    )
+);
+});
+app.get("/players/:playerId/",async(request,response)=>{
+    const{playerId}=request.params;
+    const getPlayerQuery=`
+    SELECT
+    *
+    FROM
+    player_details
+    WHERE 
+    player_id=${playerId};`;
+const player=await database.get(getPlayerQuery);
+response.send(convertPlayerDetailsDbObjectToResponseObject(player));
+});
+
+app.put("/players/:playerId/",async(request,response)=>{
+    const{playerId}=request.params;
+    const{playerName}=request.body;
+    const updatePlayerQuery=`
+    WHERE
+    player_id=${playerId};`;
+await database.run(updatePlayerQuery);
+response.send("Player Details Updated")
+});
+
+app.get("/matches/:matchId/",async(request,response)=>{
+    const {matchId}=request.params;
+    const matchDetailsQuery=`
+    SELECT
+    *
+    FROM
+      match_details
+    WHERE
+    match_id=${matchId};`;
+    const matchDetails=await database.get((matchDetailsQuery);
+    response.send(convertMatchDetailsDbObjectToResponseObject(matchDetails));
+});
+
+app.get("/players/:playerId/matches",async(request,response)=>{
+    const {playerId}=request.params;
+    const getPlayerMatchesQuery=`
+    SELECT*
+    FROM player_match_score
+     NATURAL JOIN match_details
+    WHERE 
+      player_id=${playerId};`;
+    const playerMatches=await database.all(getPlayerMatchesQuery);
+    response.send(
+        playerMatches.map((eachMatch)=>
+        convertMatchDetailsDbObjectToResponseObject(eachMatch)
+        )
+    );
+});
+
+app.get("/matches/:matchId/players",async(request,response)=>{
+    const {matchId}=request.params;
+    const getMatchPlayersQuery = `
+	SELECT
+	    player_details.player_id AS playerId,
+	    player_details.player_name AS playerName
+	FROM player_match_score NATURAL JOIN player_details
+    WHERE match_id=${matchId};`;
+const playersArray=await database.all(getMatchPlayersQuery);
+response.send(
+    playersArray.map((eachPlayer)
+    )
+);
+});
+
+app.get("/players/:playerId/playerScores",async(request,response)=>{
+    const{playerId}=request.params;
+    const getPlayerScored = `
+    SELECT
+    player_details.player_id AS playerId,
+    player_details.player_name AS playerName,
+    SUM(player_match_score.score) AS totalScore,
+    SUM(fours) AS totalFours,
+    SUM(sixes) AS totalSixes FROM 
+    player_details INNER JOIN player_match_score ON
+    player_details.player_id = player_match_score.player_id
+    WHERE player_details.player_id = ${playerId};
+    `;
+const playerMatchDetails=await database.get(getMatchPlayersQuery);
+response.send(playerMatchDetails)
+});
+
+module.exports=app;
